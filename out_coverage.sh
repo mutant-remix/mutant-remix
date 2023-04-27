32eb45469871577905c85152fcc17f78ff8d6df7
wait-what <25844213+wait-what@users.noreply.github.com>
2023-04-27 22:10:35 +0300

    Auto download json

--- out_coverage.sh
+++ out_coverage.sh
@@ -5,11 +5,13 @@ mkdir -p out/test_json
 ./orxporter/orxport.py -m assets/manifest/out.orx -j out/test_json/mtnt_test.json
 
 cp out/test_json/mtnt_test.json ./coverage/data/mtnt_test.json
+wget https://raw.githubusercontent.com/googlefonts/emoji-metadata/main/emoji_15_0_ordering.json -O ./coverage/data/emoji_15_0_ordering.json
 
 mkdir -p ./out/coverage
 
 cd coverage
+yarn
 yarn start
-cp -r ./out ../out/coverage
+cp ./out/* ../out/coverage
 
 cd "$(dirname "$0")"
