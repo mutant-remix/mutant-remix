f58a189e5a4cc32ab2aa52bf81ec706a3c72df56
wait-what <25844213+wait-what@users.noreply.github.com>
2023-04-28 05:51:37 +0300

    Update wget command

--- out_coverage.sh
+++ out_coverage.sh
@@ -5,7 +5,7 @@ mkdir -p out/test_json
 ./orxporter/orxport.py -m assets/manifest/out.orx -j out/test_json/mtnt_test.json
 
 cp out/test_json/mtnt_test.json ./coverage/data/mtnt_test.json
-wget https://raw.githubusercontent.com/googlefonts/emoji-metadata/main/emoji_15_0_ordering.json -O ./coverage/data/emoji_15_0_ordering.json
+wget https://raw.githubusercontent.com/googlefonts/emoji-metadata/main/emoji_15_0_ordering.json -P ./coverage/data
 
 mkdir -p ./out/coverage
 
