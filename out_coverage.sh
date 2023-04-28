9a6541b4f705088893712bd726d8ad01679b764a
wait-what <25844213+wait-what@users.noreply.github.com>
2023-04-28 06:08:37 +0300

    Fix mkdirs

--- out_coverage.sh
+++ out_coverage.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-mkdir -p out/test_json
+mkdir -p ./out/test_json ./coverage/data ./out/coverage
+
 ./orxporter/orxport.py -m assets/manifest/out.orx -j out/test_json/mtnt_test.json
 
 cp out/test_json/mtnt_test.json ./coverage/data/mtnt_test.json
 wget https://raw.githubusercontent.com/googlefonts/emoji-metadata/main/emoji_15_0_ordering.json -P ./coverage/data
-
-mkdir -p ./out/coverage
 
 cd coverage
 yarn
