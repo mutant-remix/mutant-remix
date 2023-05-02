94f7e8a5fbf354a0441c0c9dcf5b664ee70a9711
wait-what <25844213+wait-what@users.noreply.github.com>
2023-05-02 20:09:52 +0300

    clean: rename coverage report repository

--- out_coverage.sh
+++ out_coverage.sh
@@ -6,19 +6,19 @@ mkdir -p ./out/test_json ./coverage/data ./out/coverage
 
 # Get mutant remix emoji data
 ./orxporter/orxport.py -m assets/manifest/out.orx -j out/test_json/mtnt_test.json
-cp out/test_json/mtnt_test.json ./coverage/data/mtnt_test.json
+cp out/test_json/mtnt_test.json ./coverage-calculator/data/mtnt_test.json
 
 # Get unicode emoji data
-wget https://raw.githubusercontent.com/googlefonts/emoji-metadata/main/emoji_15_0_ordering.json -P ./coverage/data
+wget https://raw.githubusercontent.com/googlefonts/emoji-metadata/main/emoji_15_0_ordering.json -P ./coverage-calculator/data
 
 # Get inter font
 wget https://github.com/rsms/inter/releases/download/v3.19/Inter-3.19.zip -P ./coverage/data
-unzip ./coverage/data/Inter-3.19.zip -d ./coverage/data/inter
-mv ./coverage/data/inter/Inter\ Hinted\ for\ Windows/Desktop/Inter-SemiBold.ttf ./coverage/data/inter.ttf
-rm -rf ./coverage/data/inter ./coverage/data/Inter-3.19.zip
+unzip ./coverage-calculator/data/Inter-3.19.zip -d ./coverage-calculator/data/inter
+mv ./coverage-calculator/data/inter/Inter\ Hinted\ for\ Windows/Desktop/Inter-SemiBold.ttf ./coverage-calculator/data/inter.ttf
+rm -rf ./coverage-calculator/data/inter ./coverage-calculator/data/Inter-3.19.zip
 
 # Go into coverage directory
-cd coverage
+cd coverage-calculator
 
 # Install dependencies
 yarn
@@ -27,7 +27,7 @@ yarn
 yarn start
 
 # Copy the output
-cp ./out/* ../out/coverage
+cp ./out/* ../out/unicode-coverage
 
 # Go back to the root directory
 cd "$(dirname "$0")"
