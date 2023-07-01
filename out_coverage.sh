0a961aedd6003a3ec20088275b78a7a8575f3688
wait-what <25844213+wait-what@users.noreply.github.com>
2023-07-02 00:44:27 +0300

    chore: bump coverage calculator

--- out_coverage.sh
+++ out_coverage.sh
@@ -11,12 +11,6 @@ cp out/test_json/mtnt_test.json ./coverage-calculator/data/mtnt_test.json
 # Get unicode emoji data
 wget https://raw.githubusercontent.com/googlefonts/emoji-metadata/main/emoji_15_0_ordering.json -P ./coverage-calculator/data
 
-# Get inter font
-wget https://github.com/rsms/inter/releases/download/v3.19/Inter-3.19.zip -P ./coverage-calculator/data
-unzip ./coverage-calculator/data/Inter-3.19.zip -d ./coverage-calculator/data/inter
-mv ./coverage-calculator/data/inter/Inter\ Hinted\ for\ Windows/Desktop/Inter-SemiBold.ttf ./coverage-calculator/data/inter.ttf
-rm -rf ./coverage-calculator/data/inter ./coverage-calculator/data/Inter-3.19.zip
-
 # Go into coverage directory
 cd coverage-calculator
 
